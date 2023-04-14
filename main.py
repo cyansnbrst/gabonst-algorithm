f2e4f1123d63279c945b061c6730e67998af445f
Ekaterina <litra1122@gmail.com>
2023-04-14 22:31:59 +0300

    c and numba comparing

--- main.py
+++ main.py
@@ -135,6 +135,7 @@ def time_test(lang):
     functions = [f1, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f14, f15]
     for function in functions:
         if function == f1:
+            continue
             length = 10
             a = -1
             b = 1
@@ -143,34 +144,42 @@ def time_test(lang):
             a = -10
             b = 10
         elif function == f4:
+            continue
             length = 10
             a = -5
             b = 5
         elif function == f5:
+            continue
             length = 2
             a = 0
             b = 10
         elif function == f6:
+            continue
             length = 256
             a = -5.12
             b = 5.12
         elif function == f7:
+            continue
             length = 30
             a = -30
             b = 30
         elif function == f8:
+            continue
             length = 30
             a = -1.28
             b = 1.28
         elif function == f9:
+            continue
             length = 30
             a = -5.12
             b = 5.12
         elif function == f10:
+            continue
             length = 128
             a = -32.768
             b = 32.768
         elif function == f11:
+            continue
             length = 30
             a = -600
             b = 600
@@ -182,7 +191,6 @@ def time_test(lang):
             length = 2
             a = -5
             b = 5
-        start_time = time.time()
         if lang == "c":
             if function == f1:
                 function = c1
@@ -210,17 +218,20 @@ def time_test(lang):
                 function = c14
             elif function == f15:
                 function = c15
-            c_genetic_algorithm(length, 50, 50, 0.4, a, b, function_type(function))
+            start_time = time.time()
+            c_genetic_algorithm(length, 20, 20, 0.4, a, b, function_type(function))
+            end_time = time.time()
         else:
-            genetic_algorithm(length, 50, 50, 0.4, a, b, function)
-        end_time = time.time()
+            start_time = time.time()
+            genetic_algorithm(length, 20, 20, 0.4, a, b, function)
+            end_time = time.time()
         times.append(end_time - start_time)
         print(f"{function}")
     return times
 
 
-x1 = np.arange(13) - 0.2
-x2 = np.arange(13) + 0.2
+x1 = np.arange(4) - 0.2
+x2 = np.arange(4) + 0.2
 y1 = time_test("c")
 y2 = time_test("python")
 
